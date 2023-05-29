5d4637e6911ed4eb2c851a999eb5dea29b0e29e9
Đinh Việt <115639396+Dinh-Viet@users.noreply.github.com>
2023-05-30 00:25:47 +0700

    Add files via upload

--- class3/src/com/addressbooks/Contact.java
+++ class3/src/com/addressbooks/Contact.java
@@ -1,36 +1,54 @@
 package com.addressbooks;
 
+import java.util.Scanner;
+
 public class Contact {
-    private String NAME;
+        private String NAME;
 
-    private String COMPANY;
+        private String COMPAYNY;
 
-    private String EMAIL;
+        private String EMAIL;
 
-    private String PHONE_NUMBER;
+        private String PHONE_NUMBER;
 
-    public Contact(String NAME, String COMPANY, String EMAIL, String PHONE_NUMBER) {
-        this.NAME = NAME;
-        this.COMPANY = COMPANY;
-        this.EMAIL = EMAIL;
-        this.PHONE_NUMBER = PHONE_NUMBER;
-    }
+        public Contact(int anInt, String NAME, String COMPAYNY, String EMAIL, String PHONE_NUMBER) {
+            this.NAME = NAME;
+            this.COMPAYNY = COMPAYNY;
+            this.EMAIL = EMAIL;
+            this.PHONE_NUMBER = PHONE_NUMBER;
+        }
 
+        public Contact() {
 
-    public String getNAME() {
-        return NAME;
-    }
+        }
 
-    public String getCOMPANY() {
-        return COMPANY;
-    }
+        public String getNAME() {
+            return NAME;
+        }
 
-    public String getEMAIL() {
-        return EMAIL;
-    }
+        public String getCOMPAYNY() {
+            return COMPAYNY;
+        }
 
-    public String getPHONE_NUMBER() {
-        return PHONE_NUMBER;
-    }
+        public String getEMAIL() {
+            return EMAIL;
+        }
 
+        public String getPHONE_NUMBER() {
+            return PHONE_NUMBER;
+        }
+        public void inputData(){
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter Name:");
+            this.NAME = input.nextLine();
+            System.out.println("Enter Company:");
+            this.COMPAYNY = input.nextLine();
+            System.out.println("Enter Email:");
+            this.EMAIL = input.nextLine();
+            System.out.println("Enter Phone:");
+            this.PHONE_NUMBER = input.nextLine();
+        }
+        public String toString() {
+            return "Name: " + NAME + ", Company: " + COMPAYNY + ", Email: " + EMAIL + ", Phone: " + PHONE_NUMBER;
+        }
 }
--- class3/src/com/addressbooks/MainAddressbooks.java
+++ class3/src/com/addressbooks/MainAddressbooks.java
@@ -1,53 +1,58 @@
 package com.addressbooks;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class MainAddressbooks {
 
-    public static void main(String[] args) throws Exception {
+    private ArrayList<Contact> contacts = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception{
 
         ContactManagement cm = new ContactManagement();
         Scanner scanner = new Scanner(System.in);
-        boolean isMenu = false;
+        int Menu;
+        do {
+            System.out.println("1. Add new contact");
+            System.out.println("2. Find a contact by id");
+            System.out.println("3. Display contacts");
+            System.out.println("4. Exit");
 
+            Menu = scanner.nextInt();
 
-        while (!isMenu) {
-            System.out.println();
-            System.out.println("*".repeat(11) + " Welcome to Address Book program " + "*".repeat(11));
-            System.out.println("""
-                    1. Add new contact
-                    2. Find a contact by name
-                    3. Display contacts 
-                    4. Exit """);
-            System.out.print("Enter your selection: ");
-            String choice = scanner.nextLine();
-            switch (choice) {
-                case "1" -> {
-                    System.out.println();
-                    System.out.println("-".repeat(11) + " Add new contact " + "-".repeat(11));
-                    try {
-                        cm.addContact();
-                        System.out.println("Add new contact successfully");
-                    } catch (ArithmeticException e) {
-                        System.out.println("Add new contact failed" + e.getMessage());
+            switch (Menu) {
+                case 1:
+                    Contact ct = new Contact();
+                    ct.inputData();
+                    cm.addContact(ct);
+                    break;
+
+                case 2:
+                    System.out.println(" Enter id");
+                    int id=scanner.nextInt();
+
+                    Contact ps=ContactManagement.getContactById(id);
+                    if (ps!=null){
+                        System.out.println(ps.toString());
+                    }else{
+                        System.out.println("Not found");
                     }
-                }
+                    break;
 
-                case "2" -> {
-                    System.out.println();
-                    System.out.println("-".repeat(11) + " Find a contact by name " + "-".repeat(11));
-                    cm.FindContact();
-                }
-                case "3" -> {
-                    System.out.println();
-                    System.out.println("-".repeat(11) + " Find a contact by name " + "-".repeat(11));
-                    cm.DisplayContacts();
-                }
-                case "4" -> {
-                    isMenu = true;
-                }
+                case 3:
+                    ContactManagement.DisplayContact();
+                    break;
+
+                case 4:
+                    System.out.println("Exiting program...");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice.");
+                    break;
             }
-        }
+
+        } while (Menu != 4);
     }
 }
