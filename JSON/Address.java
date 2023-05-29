f8dd061d04e5ecb380221ea884336d6f29a79348
Đinh Việt <115639396+Dinh-Viet@users.noreply.github.com>
2023-05-29 23:45:04 +0700

    Add files via upload

---
+++ JSON/Address.java
@@ -0,0 +1,61 @@
+package com.JSON;
+
+public class Address {
+    String street;
+    String suite;
+    String city;
+    String zipcode;
+    Geography geo;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getSuite() {
+        return suite;
+    }
+
+    public void setSuite(String suite) {
+        this.suite = suite;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public Geography getGeo() {
+        return geo;
+    }
+
+    public void setGeo(Geography geo) {
+        this.geo = geo;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "street='" + street + '\'' +
+                ", suite='" + suite + '\'' +
+                ", city='" + city + '\'' +
+                ", zipcode=" + zipcode +
+                ", geo=" + geo +
+                '}';
+    }
+
+}
